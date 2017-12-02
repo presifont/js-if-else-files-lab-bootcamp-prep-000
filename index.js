23d72bed9badfe9b0788b01cfe4275a8d9c0cf0c
Daniel McMahon <daniel.m.mve@gmail.com>
2017-12-02 20:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-if (name === "Susan") {console.log("This is not Correct")} 
+if (name === "Susan") {console.log("This is not Correct")}
 else {console.log("This is CORRECT")}
-

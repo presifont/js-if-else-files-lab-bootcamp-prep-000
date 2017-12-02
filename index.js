e77b473efdf828774a7c0eb62e999e14ca30260f
Daniel McMahon <daniel.m.mve@gmail.com>
2017-12-02 20:42:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-if (name === "Susan") {console.log("This is not Correct")}
-else {console.log("This is CORRECT")}
+
